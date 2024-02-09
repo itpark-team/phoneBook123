44620e8af66166d36a4eef80587a3b70427d4474
amsmyslov <a.m.smyslov@yandex.ru>
2024-02-09 20:32:02 +0300

    Initial commit

---
+++ src/PhoneColors.java
@@ -0,0 +1,5 @@
+public class PhoneColors {
+    public static final String Red = "Красный";
+    public static final String Yellow = "Жёлтный";
+    public static final String SpaceGray = "Космически-Серый";
+}
